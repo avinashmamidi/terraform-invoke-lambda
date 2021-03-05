68a1b01360c0b95b9641d89cd9d6c1b314b0822b
Avinash Mamidi <avinash.mamidi548@gmail.com>
2021-03-05 17:45:24 +0530

    Update these files

--- main.tf
+++ main.tf
@@ -7,13 +7,19 @@ data "aws_lambda_invocation" "example" {
   function_name = "delete"
 
   input = jsonencode({
-    instance_id = var.key1
+    key1 = var.key1
+    key2 = var.key2
+
   })
 }
 output "result_entry" {
-  value = jsondecode(data.aws_lambda_invocation.example.result)["instance_id"]
+  value = jsondecode(data.aws_lambda_invocation.example.result)["key1"]
 }
 
 variable "key1" {
   default = "val1"
+}
+
+variable "key2" {
+  default = "val2"
 }
